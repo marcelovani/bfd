82ee6588110ec219661b34c3a746ed68113be224
OCTOGONE.dev <gregory@octogone.dev>
2020-02-08 20:21:54 +0100

    PAReview correction

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,28 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task(
+    'serve', ['sass'], function () {
 
-  browserSync.init({
-    proxy: 'http://d8-composer.dev',
-  });
+        browserSync.init(
+            {
+                proxy: 'http://d8-composer.dev',
+            }
+        );
 
-  gulp.watch("assets/scss/**/*.scss", ['sass']).on('change', browserSync.reload);
-});
+        gulp.watch("assets/scss/**/*.scss", ['sass']).on('change', browserSync.reload);
+    }
+);
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("assets/scss/style.scss")
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("assets/css"))
-    .pipe(browserSync.stream());
-});
+gulp.task(
+    'sass', function () {
+        return gulp.src("assets/scss/style.scss")
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("assets/css"))
+        .pipe(browserSync.stream());
+    }
+);
 
 gulp.task('default', ['serve']);
